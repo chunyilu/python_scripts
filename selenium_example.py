51191c97789a7e56173f866fa00c7c23435f39cf
g-stevenlue <stevenlue@gashpoint.com>
2021-11-16 10:58:14 +0800

    test

--- selenium_example.py
+++ selenium_example.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
-# go to the google home page
+# go to the google home page......
 driver.get("http://www.google.com")
 
 # the page is ajaxy so the title is originally this:
